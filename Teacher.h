aa8ade5166898c4ed89dc4ee9c2bbf051cd008e0
SharuyanArumugam <139083080+SharuyanArumugam@users.noreply.github.com>
2024-05-20 11:32:59 -0400

    Update Teacher.h

--- Teacher.h
+++ Teacher.h
@@ -1,12 +1,80 @@
-#ifndef TEACHER_H
-#define TEACHER_H
-
-class Teacher: public Person
+#include "Teacher.h"
+using namespace std;
+Teacher:: Teacher()
 {
-  public:
+  
+}
+//getters
+string Teacher::getSpecialty() 
+{
+  return specialty;
+}
+string Teacher:: getDegree()
+{
+  return degree;
+}
+char Teacher:: getStatus()
+{
+  return status;
+}
+//setters
+void Teacher:: setSpecialty(string specialty)
+{
+  this->specialty=specialty;
+}
+void Teacher:: setDegree(string degree)
+{
+  this->degree=degree;
+}
+void Teacher:: setStatus(char status)
+{
+  this->status=status;
+}
+//Payroll Calculator
+int Teacher::computePayroll(int hoursWorked, string deg) //this is for a part-time teacher
+{
+  int payroll=0;
+  if (deg == "Bachelors")
+  {
+    payroll= hoursWorked * 42 * 2 * 0.76;
+  }
+  else if (deg == "Masters")
+  {
+    payroll= hoursWorked * 82 * 2 * 0.76;
+  }
+  else if (deg == "PhD")
+  {
+    payroll= hoursWorked * 112 * 2 * 0.76;
+  }
+  else
+    ;
+  return payroll;
+}
 
-  private:
+int Teacher::computePayroll(string deg) //this is for a full-time teacher
+{
+  int payroll=0;
+  if (deg == "Bachelors")
+  {
+    payroll= 32 * 42 * 2 * 0.85;
+  }
+  else if (deg == "Masters")
+  {
+    payroll= 32 * 82 * 2 * 0.85;
+  }
+  else if (deg == "PhD")
+  {
+    payroll= 32 * 112 * 2 * 0.85;
+  }
+  else
+    ;
+  return payroll;
+}
 
-};
+Teacher:: Teacher(string x, string y, char z) //Input Teacher Info
+{
+  x=specialty;
+  y=degree;
+  z= status;
+}
 
-#endif
